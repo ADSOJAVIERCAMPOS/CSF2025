cc0334238c3749be01118f26f4117584154f967b
ADSOJAVIERCAMPOS <155128371+ADSOJAVIERCAMPOS@users.noreply.github.com>
2025-04-01 14:33:50 -0500

    Update EmpleadoRepositorio.java

--- Abril2025/nexos /src/main/java/com/nexos/repositorio/EmpleadoRepositorio.java
+++ Abril2025/nexos /src/main/java/com/nexos/repositorio/EmpleadoRepositorio.java
@@ -1,5 +1,8 @@
 package com.nexos.repositorio;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,14 @@ import com.nexos.modelos.Empleado;
 
 @Repository
 public interface EmpleadoRepositorio extends JpaRepository<Empleado, Long> {
+
+      @Override
+    List<Empleado> findAll();
+
+    @Override
+    Optional<Empleado> findById(Long id);
+
+    @Override
+    Empleado save(Empleado empleado);
+  
 }
